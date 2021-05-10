d03efb1a78d8dfd170c623237b74b1ded8374da7
Neal Maze <nealwmaze@gmail.com>
2021-05-10 03:56:29 -0600

    y

--- csv_importer.py
+++ csv_importer.py
@@ -1,4 +1,21 @@
 # imports
 from importerHelpers import *
 
-dir = ("C:/Users/LuciusFish/Desktop/moto_csv")
+baseDir = ("C:/Users/LuciusFish/Desktop/moto_csv/")
+types = ["RAC", "RAC2", "Q2", "Q1", "WUP", "FP1", "FP2", "FP3", "FP4", "Test"]
+
+finFiles = []
+with open("C:/Users/LuciusFish/Desktop/csv/finFiles.txt", "r") as f:
+    contents = f.readlines()
+    for i in contents:
+        finFiles.append(i)
+
+for i in finFiles:
+    if i == 0 or i == []:
+        del i
+
+for sesType in Types:
+    dir = baseDir + sesType
+    with
+
+
